9ea95f657622449a97049a67e50dae7bf2a99120
Lupitaqs <lupitaqs@outlook.com>
2024-05-14 12:33:54 -0600

    Serve a static file in myApp.js

--- myApp.js
+++ myApp.js
@@ -14,7 +14,8 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
+// Middleware to serve static files
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
