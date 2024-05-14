daa56d25858d12a40b8943c069c3d6db690fbc55
Lupitaqs <lupitaqs@outlook.com>
2024-05-14 11:59:48 -0600

    Sent a file to browser in myApp.js

--- myApp.js
+++ myApp.js
@@ -5,11 +5,14 @@ let app = express();
 
 console.log("Hello World")
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// });
+
+// Send a file to the browser
+app.get("/", function(req, res) {   
+    res.sendFile(__dirname + "/views/index.html");
 });
-
-
 
 
 
